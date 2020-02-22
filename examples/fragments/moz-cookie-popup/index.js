44da9a4758e8819a5982973de0b8aa5a073ca49a
dikyarga <dikyarga.id@gmail.com>
2020-02-22 15:13:18 +0700

    add tailwindcss style

--- examples/fragments/moz-cookie-popup/index.js
+++ examples/fragments/moz-cookie-popup/index.js
@@ -1,12 +1,18 @@
 new Vue({
   el: "#moz-cookie-popup",
   template: `
-  <div>
-    <h3 class="title">
-      Apakah Anda mau setiap aktivitas Anda dilacak?
-    </h3>
-    <button @click="confirm">Mau</button>
-    <button>Terpaksa mau</button>
+  <div class="flex w-full absolute bottom-0 left-0 justify-center">
+    <div class="bg-gray-100 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <h3 class="text-2xl mb-4">Apakah setiap aktivitas Anda ingin dilacak?</h3>
+      <div class="flex items-center justify-center">
+        <button class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 mr-2 rounded focus:outline-none focus:shadow-outline" type="button" @click="confirm">
+          Mau, lacak saya
+        </button>
+        <a class="inline-block align-baseline font-bold text-sm text-orange-500 ml-2 hover:text-orange-800" href="#">
+          tutup website ini
+        </a>
+      </div>
+    </div>
   </div>
   `,
   data() {
